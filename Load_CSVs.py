f6c8703c5c98941797138d4fa3dae94294c52be2
Skyler-18 <154238492+Skyler-18@users.noreply.github.com>
2024-05-03 08:21:06 +0530

    Update Load_CSVs.py

--- Load_CSVs.py
+++ Load_CSVs.py
@@ -2,9 +2,11 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 conn_string = 'postgresql://postgres:Postgres@localhost/Paintings'
+# 'postgresql://<username>:<password>@localhost/<database name>'
 db = create_engine(conn_string)
 conn = db.connect()
 
+# name of the csv files in the folder name given to be added to the database
 tables = ['artist', 'canvas_size', 'image_link', 'museum_hours', 'museum', 'product_size', 'subject', 'work']
 
 for table in tables:
